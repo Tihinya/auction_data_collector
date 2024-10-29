eaa9f2dd686b2812a32f20cc3ecb4600904873c5
Tihinya <tikhinya.stepan@gmail.com>
2024-10-29 19:53:25 +0200

    Add configuration management setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"auction-data-collector/internal/config"
+	"fmt"
+	"log"
+)
+
 func main() {
 
+	cfg, err := config.LoadConfig("config/config.json")
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+	fmt.Println(cfg)
 }
